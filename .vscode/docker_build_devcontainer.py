82e796c69d4926f7ad02e7d3fdb633df9c0604e1
kakkoii1337 <kakkoii1337@gmail.com>
2024-08-31 13:33:44 +0800

    initial create

--- .vscode/docker_build_devcontainer.py
+++ .vscode/docker_build_devcontainer.py
@@ -1,7 +1,7 @@
 import toml,os,subprocess
 
 repo_name="kakkoii/"
-base_name="gai-torch2.2.0-cuda12.1-ubuntu22.04-builder"
+base_name="gai-torch2.2.0-cuda12.1-ubuntu22.04-devcontainer"
 
 ## Update version in pyproject.toml
 def __update_version(pyproject_path):
@@ -24,15 +24,16 @@ def __update_version(pyproject_path):
 def main():
     here=os.path.dirname(__file__)
     pyproject_dir=os.path.join(here,'..')
-    dockerfile_dir=pyproject_dir
     pyproject_path=os.path.join(pyproject_dir,'pyproject.toml')
+    dockerfile_dir=os.path.abspath(os.path.join(pyproject_dir,'.devcontainer'))
+    context_dir = pyproject_dir
 
     # Get the version from the pyproject.toml file
     version = __update_version(pyproject_path=pyproject_path)
     print(f"Building docker image with version: {version}")
 
     # Build the docker image
-    os.system(f"docker build -t {repo_name}{base_name}:{version} {dockerfile_dir}")
+    os.system(f"docker build -t {repo_name}{base_name}:{version} -f {dockerfile_dir}/Dockerfile.devcontainer {context_dir}")
     os.system(f"docker tag {repo_name}{base_name}:{version} {repo_name}{base_name}:latest")
 
 if __name__ == "__main__":
